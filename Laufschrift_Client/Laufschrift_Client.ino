99323907acc1805f3eafccc7f7309d0858172b83
Lennotrix <67200846+Lennotrix@users.noreply.github.com>
2024-01-22 14:00:28 +0100

    Added Header and JWT

--- Laufschrift_Client/Laufschrift_Client.ino
+++ Laufschrift_Client/Laufschrift_Client.ino
@@ -8,7 +8,6 @@
 #include <HTTPClient.h>
 
 #include <Arduino_JSON.h>
-#include <assert.h>
 
 #include <MD_Parola.h>
 #include <MD_MAX72xx.h>
@@ -17,18 +16,23 @@
 #define MAX_DEVICES 4  // 4 blocks
 #define CS_PIN 21
 
+#define DEBUG
+
 // create an instance of the MD_Parola class
 MD_Parola ledMatrix = MD_Parola(HARDWARE_TYPE, CS_PIN, MAX_DEVICES);
 
 WiFiMulti wifiMulti;
 
-const char* ssid = "itao";
-const char* pw = "kleinholz";
-char meinTextPuffer[HTTP_MAX_LEN + 1] = "Hier könnte Ihr Text stehen!";
-
+const char* ssid = "TSST-SN";
+const char* pw = "123456789";
+char meinTextPuffer[HTTP_MAX_LEN + 1] = { '\0' };
+char* pPhraseURL = "http://10.1.1.30:5047/api/v01/phrase?id=2";
+const char* pLoginURL = "http://10.1.1.30:5047/api/v01/Auth/Login";
+const char* uName = "test001";
+const char* uPw = "test001";
+http myHttp(uName, uPw);
 
 void setup() {
-
   Serial.begin(19200);
 
   wifiMulti.addAP(ssid, pw);
@@ -36,26 +40,21 @@ void setup() {
   if ((wifiMulti.run() == WL_CONNECTED)) {
     Serial.println("Connected");
   }
-
   ledMatrix.begin();           // initialize the object
   ledMatrix.setIntensity(15);  // set the brightness of the LED matrix display (from 0 to 15)
   ledMatrix.displayClear();    // clear led matrix display
   ledMatrix.displayScroll(meinTextPuffer, PA_CENTER, PA_SCROLL_LEFT, 50);
+  //http myHttp(uName, uPw);
 }
 
 void loop() {
-  http myHttp("http://10.1.122.19:7115/api/v01/phrase");
-
-  char* displayText = myHttp.GetPhrase();
-  fillTextBuffer(displayText);
-  Serial.println(displayText);
   animate();
-  //delay(3000);
-  //http.end();
 }
 
 void animate() {
   if (ledMatrix.displayAnimate()) {
+    char* displayText = myHttp.GetPhrase(pPhraseURL);
+    fillTextBuffer(displayText);
     ledMatrix.displayReset();
   }
 }
@@ -76,10 +75,10 @@ char sonderzeichen195(char code) {
       ergebnis = 196;
       break;
     case 156:
-        ergebnis = 220;
+      ergebnis = 220;
       break;
     case 150:
-        ergebnis = 214;
+      ergebnis = 214;
       break;
     default:
       Serial.print("Unbekanntes 195-Zeichen --> ");
--- Laufschrift_Client/http.cpp
+++ Laufschrift_Client/http.cpp
@@ -2,70 +2,101 @@
 #include <Arduino.h>
 #include <HTTPClient.h>
 #include <Arduino_JSON.h>
-#include <assert.h>
 
-#define DEBUG_OUT
+//#define DEBUG_OUT
 //#undef DEBUG_OUT
 
 // Variablen
 
 char buffer[HTTP_MAX_LEN + 1] = { '\0' };
+char bearerBuffer[HTTP_MAX_LEN + 1] = { '\0' };
 
-http::http(char* endpoint) {
-  this->endpoint = endpoint;
+http::http(const char* uName, const char* pw) {
+  this->uName = uName;
+  this->uPw = pw;
 }
 
-http::http(char* endpoint, char* token) {
-  this->endpoint = endpoint;
-  this->token = token;
-}
-
-http::http(char* endpoint, char* testJSON, int bum) {
-  this->endpoint = endpoint;
-  this->testJSON = testJSON;
-}
 http::~http() {
 }
 
-char* http::GetPhrase() {
+char* http::GetPhrase(char* endpoint) {
 
-  return http::Get();
-}
-
-char* http::Get() {
   char* phrase = &buffer[0];
+  *phrase = '\0';
   bool anonym;
-  buffer[0] = '\0';
+
   HTTPClient httpCli;
-  Serial.println(endpoint);
   httpCli.begin(endpoint);  //HTTP
+  httpCli.addHeader("Content-Type", "application/json");
+  httpCli.addHeader("Authorization", pBearer);
+
   int httpCode = httpCli.GET();
+  Serial.println(httpCode);
+  if (httpCode == 401) {
+    this->pBearer = Login();
+    return phrase;
+  }
 
   JSONVar jsonObject = JSON.parse(httpCli.getString().c_str());
   if (JSON.typeof(jsonObject) == "undefined") {
     Serial.println("Parsing input failed!");
-  } else {
-    Serial.println(jsonObject);
-    if (jsonObject.hasOwnProperty("anonym")) {
-      anonym = (bool)jsonObject["anonym"];
-    }
-    if (jsonObject.hasOwnProperty("class") && !anonym) {
-      
-      strncat(phrase, ((const char*)jsonObject["class"]), (HTTP_MAX_LEN - strlen(phrase)));
+    return phrase;
+  }
+  Serial.println(jsonObject);
+  if (jsonObject.hasOwnProperty("anonym")) {
+    anonym = (bool)jsonObject["anonym"];
+  }
+  if (!anonym) {
+    if (jsonObject.hasOwnProperty("creatorClassName")) {
+
+      strncat(phrase, ((const char*)jsonObject["creatorClassName"]), (HTTP_MAX_LEN - strlen(phrase)));
       strncat(phrase, ": ", (HTTP_MAX_LEN - strlen(phrase)));
     }
-    if (jsonObject.hasOwnProperty("firstName") && !anonym) {
-      strncat(phrase, ((const char*)jsonObject["firstName"]), (HTTP_MAX_LEN - strlen(phrase)));
+    if (jsonObject.hasOwnProperty("creatorFirstName")) {
+      strncat(phrase, ((const char*)jsonObject["creatorFirstName"]), (HTTP_MAX_LEN - strlen(phrase)));
       strncat(phrase, ": ", (HTTP_MAX_LEN - strlen(phrase)));
-    }
-    if (jsonObject.hasOwnProperty("phrase")) {
-      strncat(phrase, ((const char*)jsonObject["phrase"]), (HTTP_MAX_LEN - strlen(phrase)));
     }
   }
+  if (jsonObject.hasOwnProperty("phrase")) {
+    strncat(phrase, ((const char*)jsonObject["phrase"]), (HTTP_MAX_LEN - strlen(phrase)));
+  }
+
   httpCli.end();
-#ifdef DEBUG_OUT  
-  Serial.print("  ---> Ergebnis: ");
-  Serial.println(phrase);
-#endif  
+  
   return phrase;
+}
+
+char* http::Login() {
+  const char* endpoint = "http://10.1.1.30:5047/api/v01/auth/Login";
+  HTTPClient http;
+
+  http.begin(endpoint);
+
+  http.addHeader("Content-Type", "application/json");
+
+  char* pBearer = &bearerBuffer[0];
+  *pBearer = '\0';
+
+  JSONVar doc;
+
+  doc["userName"] = this->uName;
+  doc["password"] = this->uPw;
+
+  int httpCode = http.POST(JSON.stringify(doc));
+
+  if (httpCode > 0) {
+
+    // file found at server
+    if (httpCode == HTTP_CODE_OK) {
+      const char* payload = http.getString().c_str();
+      JSONVar jsonObject = JSON.parse(payload);
+
+      strncat(pBearer, "Bearer ", (HTTP_MAX_LEN - strlen(pBearer)));
+      strncat(pBearer, ((const char*)jsonObject["args"]["Bearer"]), (HTTP_MAX_LEN - strlen(pBearer)));
+    }
+  } else {
+    Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+  }
+  http.end();
+  return pBearer;
 }
